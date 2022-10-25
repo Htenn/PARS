4ff79559efcccfa7ef58568e7f4bb86dd4aa1afa
neilong15 <neil12267@gmail.com>
2022-10-25 20:14:34 +0800

    remove submit button

--- assets/includes/addform.php
+++ assets/includes/addform.php
@@ -7,25 +7,25 @@
         <!-- Text -->
         <!-- Form -->
             <section>
-                <form method="post" action="add_client-config.php">
+                <form method="post" action="add_passenger-config.php">
                     <div class="row gtr-uniform">
                         <div class="col-4 col-12-xsmall">
                             <h2>First Name</h2>
-                            <input type="text" name="clientFirstName" id="clientFirstName" value="" placeholder="First Name" required/>
+                            <input type="text" name="passengerFirstName" id="passengerFirstName" onchange="saveValue(this)" value="" placeholder="First Name" required/>
                         </div>
                         <div class="col-4 col-12-xsmall">	
                             <h2>Middle Name</h2>
-                            <input type="text" name="clientMiddleName" id="clientMiddleName" value="" placeholder="Middle Name" />
+                            <input type="text" name="passengerMiddleName" id="passengerMiddleName" onchange="saveValue(this)" value="" placeholder="Middle Name" />
                         </div>
                         <div class="col-4 col-12-xsmall">
                             <h2>Last Name</h2>
-                            <input type="text" name="clientLastName" id="clientLastName" value="" placeholder="Last Name" required/>
+                            <input type="text" name="passengerLastName" id="passengerLastName" onchange="saveValue(this)" value="" placeholder="Last Name" required/>
                         
                         </div>
                         
                         <div class="col-6">
                             <h2>Gender</h2>
-                            <select name="clientGender" id="clientGender">
+                            <select name="passengerGender" onchange="saveValue(this)" id="passengerGender" >
                                 <option value="1">Male</option>
                                 <option value="2">Female</option>
                                 
@@ -35,48 +35,40 @@
                         
                         <div class="col-6 col-12-xsmall">
                             <h2>Birthdate</h2>
-                            <input type="date" name="clientBirthday" id="clientBirthday" value="" placeholder="MM/DD/YY" />
+                            <input type="date" name="passengerBirthday" id="passengerBirthday" onchange="saveValue(this)" value="" placeholder="MM/DD/YY" />
                         </div>
 
                         
                         <div class="col-6 col-12-xsmall">
                             <h2>Age</h2>
-                            <input type="text" name="clientAge" id="clientAge" value="" placeholder="Age" />
+                            <input type="text" name="passengerAge" id="passengerAge" onchange="saveValue(this)" value="" placeholder="Age" />
                         </div>
 
                         <div class="col-6 col-12-xsmall">
                             <h2>Email</h2>
-                            <input type="email" name="clientEmail" id="clientEmail" value="" placeholder="Email" />
+                            <input type="email" name="passengerEmail" id="passengerEmail" onchange="saveValue(this)" value="" placeholder="Email" />
                         </div>
 
                         <div class="col-6 col-12-xsmall">
                             <h2>Contact Number</h2>
-                            <input type="text" name="clientContactNum" id="clientContactNum" value="" placeholder="************" />
+                            <input type="text" name="passengerContactNum" id="passengerContactNum" onchange="saveValue(this)" value="" placeholder="************" />
                         </div>
                         <div class="col-6 col-12-xsmall">
                             <h2>Nationality</h2>
-                            <input type="text" name="clientNationality" id="clientNationality" value="" placeholder="Nationality" required/>
+                            <input type="text" name="passengerNationality" id="passengerNationality" onchange="saveValue(this)" value="" placeholder="Nationality" required/>
                         </div>
-                        
-                        <div class="col-6 col-12-xsmall">
-                            <h2>Remarks</h2>
-                            <input type="text" name="clientRemarks" id="clientRemarks" value="" placeholder="Remarks" />
-                        </div>
+
                         <div class="col-6 col-12-xsmall">
                             <h2>Passenger Type</h2>
-                            <select name="clientType" id="clientType">
+                            <select name="passengerType" id="passengerType">
                                 <option value="Normal">Normal</option>
+                                <option value="Infant">Infant</option>
                                 <option value="Unaccompanied Minor">Unaccompanied Minor</option>
                                 <option value="Handicapped">Handicapped</option>
                                 <option value="Medically OK for travel">Medically OK for travel</option>
                                 <option value="Senior Citizen">Senior Citizen</option>
                                 
                             </select>
-                        </div>
-                        <div class="col-6 col-12-xsmall">
-                            <h2>Additional Passenger (If none, type "0")</h2>
-                            <input type="text" name="clientAddPass" id="clientAddPass" value="" placeholder="Number of Passengers " />
-                            
                         </div>
                         
                         

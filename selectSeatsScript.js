0ffba83b6d39969a55aebe83e98dbfa928cb9f1c
Htenn <santossean36@gmail.com>
2022-11-17 17:02:53 +0800

    ammend some changes in selectsSeats js
    (still incomplete)

--- selectSeatsScript.js
+++ selectSeatsScript.js
@@ -1,8 +1,32 @@
 const container = document.querySelector(".container");
 const seats = document.querySelectorAll(".row .seat:not(.sold)");
 
-// Update total and count
+populateUI();
 
+// Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
+    var ids = $(".seat.selected").map(function() {
+        return this.id;
+    }).get();
+}
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+
+    if (selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                console.log(seat.classList.add("selected"));
+            }
+        });
+    }
+}
 
 // Seat click event
 container.addEventListener("click", (e) => {
@@ -16,10 +40,5 @@ container.addEventListener("click", (e) => {
     }
 });
 
-function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll(".row .seat.selected");
-
-    const seatsIndex = $(".row .seat.selected").map(function() {
-        return this.id;
-    }).get();
-}
+//initial count and total set
+updateSelectedCount();

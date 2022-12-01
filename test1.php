e36cd5da13f5defce1e33868f04a2db6584c1825
Htenn <santossean36@gmail.com>
2022-12-01 21:57:51 +0800

    remove test file

--- test1.php
+++
@@ -1,9 +0,0 @@
-<?php
-    date_default_timezone_set("Asia/Manila");
-    echo "The time is " . date("h:i:sa");
-
-    if (isset($_GET['submit'])) {
-        $elements = $_GET['str'];
-        echo $elements;
-    }
-?>

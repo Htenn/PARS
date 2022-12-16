e410d739703969e1537bf1cf8eb2a27f04b23767
Htenn <santossean36@gmail.com>
2022-12-16 21:38:04 +0800

    make menu button dynamic

--- includes/menubutton.php
+++ includes/menubutton.php
@@ -1,5 +1,13 @@
+<?php
+    if($_SESSION['session'] == 'A') {
+        $menu = 'adminmenu.php';
+    } else {
+        $menu = 'mainmenu.php';
+    }
+?>
+
 <div style='position: fixed; float: left; margin-left: 15px; margin-top: 15px; color: grey;'>
     <ul class='actions'>
-        <li><a href="mainmenu.php" class="button primary small">Menu</a></li>
+        <li><a href="<?php echo $menu;?>" class="button primary small">Menu</a></li>
     </ul>
 </div>

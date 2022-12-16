e3a3eca1d10f61ef2433c77334a26d4e833f7e28
Htenn <santossean36@gmail.com>
2022-12-16 21:31:13 +0800

    new menu button for the general UI

---
+++ includes/menubutton.php
@@ -0,0 +1,5 @@
+<div style='position: fixed; float: left; margin-left: 15px; margin-top: 15px; color: grey;'>
+    <ul class='actions'>
+        <li><a href="mainmenu.php" class="button primary small">Menu</a></li>
+    </ul>
+</div>
